84d8f263e2a844ab6ebb75e3401a0e99be090f8a
AnfimovArtyom <anfimovartyom@yandex.ru>
2021-03-22 13:24:32 +0300

    Добавьте файлы проекта.

---
+++ HelloWorld.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+// This function prints
+void print()
+{
+    // вызываем оператор << на объекте типа std::cout
+    std::cout << "Hello Skillbox!\n";
+}
+
+/*
+ Приписываем значение переменной и печатаем Hello world!
+*/
+int main()
+{
+    int x = 100;
+    3 + 7;
+    int y = x + 100;
+
+    int b = 0;
+    b = b + 2;
+
+    int test;
+    test = 100500;
+
+    int test2 = 1005001;
+
+
+
+
+    int mult = x * y;
+    int random = 1002;
+
+    std::cout << random;
+}
